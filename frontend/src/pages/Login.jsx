3a8b0aea1335e2fbe497b30ca036c044c2c70bb7
JochenSunaert <Jochen.sunaert@student.ehb.be>
2025-08-06 17:45:30 +0200

    feat: user signup form created using supabase

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      {/* Other components or content can be added here */}
+    </div>
+  );
+}
+
+export default App;
